8f3b4b999189ed7c380794e8f5b662ec8ea8e5d1
Zandacrus <94130218+Zandacrus@users.noreply.github.com>
2022-10-09 04:07:51 +0530

    Registers Package (Complete)
    Register state functionility added for special registers (except zero register, since it can never be written). Some other minor changes are made. Any previously declared function is not modified.

--- Singlethread Microprocessor/Phase 2 - Register Fetch Unit/Register Memory Section/Registers.sv
+++ Singlethread Microprocessor/Phase 2 - Register Fetch Unit/Register Memory Section/Registers.sv
@@ -5,7 +5,7 @@
 package REGISTERS;
 	reg [28:0][31:0] registers; // 29 registers each of 32 bit
 	reg [2:0][31:0] special_registers = '{3{32'b0}}; // 3 special registers each of 32 bit (zero(0), lo(1), hi(2))
-	reg [28:0][2:0] state = '{29{3'b0}}; // state[i] represents whether register i is busy(>0) or not (0) 
+	reg [30:0][2:0] state = '{31{3'b0}}; // state[i] represents whether register i is busy(>0) or not (0) 
 	// max possible value of state[i] is 5 since it is a 5 stage pipelined architecture
 	
 	function logic [31:0] read_reg_data (logic [4:0] reg_num);
@@ -15,8 +15,8 @@ package REGISTERS;
 	endfunction
 	
 	function logic [2:0] read_reg_state (logic [4:0] reg_num);
-		if ((reg_num>'b0)&&(reg_num<'d30)) return state[(reg_num-'b1)];
-		else return 'b0;
+		if (reg_num>'b0) return state[(reg_num-'b1)];
+		else return 3'b0;
 	endfunction
 	
 	function void set_reg_state(logic [4:0] reg_num);
